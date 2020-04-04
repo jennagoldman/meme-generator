08a8107726f4d475c4944d5da3d5cb6ce63712d1
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-03 22:31:57 -0700

    add POST route and test for creating a meme

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -5,7 +5,7 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
-describe('app routes', () => {
+describe('meme routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -16,5 +16,24 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('creates a meme', () => {
+    return request(app)
+      .post('/api/v1/memes')
+      .send({
+        top: 'I don\'t think that memes',
+        image: 'https://pbs.twimg.com/profile_images/1039262742237118465/J3yZraiI.jpg',
+        bottom: 'what you think it memes'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          top: 'I don\'t think that memes',
+          image: 'https://pbs.twimg.com/profile_images/1039262742237118465/J3yZraiI.jpg',
+          bottom: 'what you think it memes',
+          __v: 0
+        });
+      });
   });
 });
